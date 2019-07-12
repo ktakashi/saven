096072c3e50e6847272b582e0565079d533260f8
Takashi Kato <ktakashi@ymail.com>
2019-07-12 16:25:02 +0200

    Adding skelton for phase

--- lib/saven.scm
+++ lib/saven.scm
@@ -1,6 +1,10 @@
 ;;; -*- mode: scheme; coding: utf-8 -*-
 #!nounbound
 (library (saven)
-    (export saven:lifecycle)
-    (import (saven lifecycle)))
+    (export saven:build-file->module-descriptor
+	    saven:analyse-descriptor
+	    saven:lifecycle)
+    (import (saven descriptors)
+	    (saven analyse)
+	    (saven lifecycle)))
 
--- lib/saven/lifecycle.scm
+++ lib/saven/lifecycle.scm
@@ -3,13 +3,68 @@
 (library (saven lifecycle)
     (export saven:lifecycle)
     (import (rnrs)
-	    (saven descriptors)
-	    (saven analyse))
+	    (saven descriptors))
 
-(define (saven:lifecycle sav-file . targets)
-  (define descriptor (saven:build-file->module-descriptor sav-file))
-  (define sorted-modules (saven:analyse-descriptor descriptor))
-  (display (map saven:module-descriptor-name sorted-modules)) (newline)
-  sorted-modules)
+(define (saven:lifecycle modules)
+  (define executions (map saven:module->execution modules))
+  (lambda (targets)
+    (fold-left (lambda (results execution) (cons (execution targets) results))
+	       '() executions)))
+
+(define (saven:module->execution module)
+  (lambda (targets)
+    (display (saven:module-descriptor-name module)) (newline)))
+
+;; phase and builtin target mapping
+;; - pre-build   +---+
+;;               |   |
+;; - build       +---+-- build
+;;               |   |
+;; - post-build  +---+
+;;               |
+;; - pre-test    +---+
+;;		 |   |
+;; - test	 +---+-- test
+;;		 |   |
+;; - post-test	 +---+
+;;               |
+;; - package     +------ package
+;; 
+;;                       clean (clean doesn't have any phase)
+;; 
+;; users will be able to extend phase behaviour (not now)
+
+;;; Phases
+;; build
+;; TODO check 'build' and 'plugin'
+(define (saven-phase:pre-build module)
+  ;; TODO collect dependencies and compute load path
+  ;; NB: dependencies will be downloaded into '.sav/dep/main' directory
+  )
+(define (saven-phase:build module)
+  ;; TODO build it wiht the pre-build result
+  )
+(define (saven-phase:post-build module)
+  ;; future hook
+  )
+
+;; test
+(define (saven-phase:pre-test module)
+  ;; TODO collect dependencies and compute load path
+  ;; NB: dependencies will be downloaded into '.sav/dep/test' directory
+  )
+(define (saven-phase:test module)
+  ;; TODO build it wiht the pre-build result
+  )
+(define (saven-phase:post-test module)
+  ;; future hook
+  )
+
+;; package
+(define (saven-phase:package module)
+  ;; TODO collect source into one directory if necessary
+  )
+
+
 )
 	    
