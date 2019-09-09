ec85559f25065d6aaf5d72b1251b656c5dd7abbd
Takashi Kato <ktakashi@ymail.com>
2019-09-09 15:41:06 +0200

    Searching supporting extension instead of only sav.yaml

--- lib/saven.scm
+++ lib/saven.scm
@@ -3,8 +3,10 @@
 (library (saven)
     (export saven:build-file->module-descriptor
 	    saven:analyse-descriptor
-	    saven:lifecycle)
+	    saven:lifecycle
+	    saven:supported-file-extensions)
     (import (saven descriptors)
 	    (saven analyse)
-	    (saven lifecycle)))
+	    (saven lifecycle)
+	    (saven build-file)))
 
